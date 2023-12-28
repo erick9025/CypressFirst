fef2a1e40626aa012bfbeedc90085a7fbb53a0d4
Erick Jimenez <Erick.Jimenez@coxautoinc.com>
2023-12-28 13:51:15 -0600

    Updated Google search tests names

--- cypress-e2e-framework/cypress/e2e/integrations/0-erick/google_search_erick.cy.js
+++ cypress-e2e-framework/cypress/e2e/integrations/0-erick/google_search_erick.cy.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 //Author: Erick Eduardo Jiménez Rodríguez
 
-describe('erick simple google searches', () => {
+describe('Erick simple google searches', () => {
   beforeEach(() => {
     cy.visit('https://google.com')
   })
 
-  it('Search some string A', () => {
+  it('First search', () => {
     cy.get('[name=q]').type(`dua lipa{enter}`)
   })
 
-  it('Search some string B', () => {
+  it('Second search', () => {
     cy.get('[name="q"]').type(`erick9025{enter}`)
   })  
 })
